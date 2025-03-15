0d4af4214663d406235a0592cff85ebda883e3a9
Shubham Prashar <shubhamprasharr40@gmail.com>
2025-03-15 17:59:13 -0600

    header section

---
+++ app/header/page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+    return (
+        <header>
+            <span className="logo"><a href="/">SharpDeals</a></span>
+
+            <nav>
+                <ul>
+                    <li><a href="#hotdeals">HotDeals</a></li>
+                    <li><a href="#budgeting">Smart Budgeting</a></li>
+                    <li><a href="#shop">Shop with Friend</a></li>
+                </ul>
+            </nav>
+
+            <div className="auth-buttons">
+                <a href="#login" className="login-btn">Login</a>
+                <a href="#signup" className="signup-btn">Sign Up</a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
